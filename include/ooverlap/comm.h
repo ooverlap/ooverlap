a3e75a7f90f9e860b85610ed25875f5c74530b13
keyvan-dadash <ddssee5511@gmail.com>
2026-05-03 15:10:54 +0200

    refactoe: add the reduce-scatter and all-gather to the public apis

--- include/ooverlap/comm.h
+++ include/ooverlap/comm.h
@@ -28,11 +28,7 @@ typedef enum {
 
 typedef enum {
     OO_REDUCE_ADD = 0,
-
-    // Alias for users coming from NCCL-style naming.
-    // Sum and add are the same operation for this collective.
     OO_REDUCE_SUM = OO_REDUCE_ADD,
-
     OO_REDUCE_MIN = 1,
     OO_REDUCE_MAX = 2
 } oo_reduce_op_t;
@@ -47,13 +43,33 @@ typedef enum {
     OO_TUNING_BEST_EFFICIENCY = 1
 } oo_tuning_mode_t;
 
+typedef struct {
+    size_t element_offset;
+    size_t count;
+    void* ptr;
+} oo_tensor_slice_t;
+
 /* Utility */
 size_t oo_dtype_size(
     oo_dtype_t dtype);
 
+oo_status_t oo_rank_partition(
+    int rank,
+    int world_size,
+    size_t count,
+    size_t* out_element_offset,
+    size_t* out_count);
+
 int oo_allreduce_supported(
     oo_dtype_t dtype,
     oo_reduce_op_t op);
+
+int oo_reduce_scatter_supported(
+    oo_dtype_t dtype,
+    oo_reduce_op_t op);
+
+int oo_all_gather_supported(
+    oo_dtype_t dtype);
 
 /* Group */
 oo_status_t oo_group_create(
@@ -123,33 +139,33 @@ size_t oo_buffer_mapped_bytes(
 oo_buffer_kind_t oo_buffer_kind(
     const oo_buffer_t* buffer);
 
-/* Collectives */
-/* IPC / synchronization helpers */
+/* IPC / synchronization */
 oo_status_t oo_group_sync(
     oo_group_t* group);
 
 /*
- * For multiprocess IPC groups:
- * - exports this rank's local wrapped/allocation buffer through the group's broker
- * - imports the peer rank's buffer
- * - returns an oo_buffer_t for the peer buffer
+ * Exchange this rank's local buffer with all remote ranks.
  *
- * This is intended for externally allocated CUDA buffers, e.g. PyTorch tensors
- * wrapped with oo_buffer_wrap().
+ * out_peers must have space for:
+ *
+ *   oo_group_size(oo_node_group(node)) - 1
+ *
+ * entries.
+ *
+ * out_peer_count receives the number of peer buffers written.
  */
-oo_status_t oo_buffer_exchange_ipc_peer(
+oo_status_t oo_buffer_exchange_ipc_peers(
     oo_node_t* node,
     oo_buffer_t* local,
-    oo_buffer_t** out_peer);
+    oo_buffer_t** out_peers,
+    int* out_peer_count);
 
-/*
- * Run allreduce on a contiguous slice of already-registered local/peer buffers.
- * element_offset and count are in elements, not bytes.
- */
+/* Allreduce */
 oo_status_t oo_allreduce_offset(
     oo_node_t* node,
     oo_buffer_t* local,
-    oo_buffer_t* peer,
+    oo_buffer_t* const* peers,
+    int peer_count,
     size_t element_offset,
     size_t count,
     oo_dtype_t dtype,
@@ -159,7 +175,8 @@ oo_status_t oo_allreduce_offset(
 oo_status_t oo_allreduce(
     oo_node_t* node,
     oo_buffer_t* local,
-    oo_buffer_t* peer,
+    oo_buffer_t* const* peers,
+    int peer_count,
     size_t count,
     oo_dtype_t dtype,
     oo_reduce_op_t op,
@@ -168,7 +185,8 @@ oo_status_t oo_allreduce(
 oo_status_t oo_allreduce_offset_tuned(
     oo_node_t* node,
     oo_buffer_t* local,
-    oo_buffer_t* peer,
+    oo_buffer_t* const* peers,
+    int peer_count,
     size_t element_offset,
     size_t count,
     oo_dtype_t dtype,
@@ -179,10 +197,101 @@ oo_status_t oo_allreduce_offset_tuned(
 oo_status_t oo_allreduce_tuned(
     oo_node_t* node,
     oo_buffer_t* local,
-    oo_buffer_t* peer,
+    oo_buffer_t* const* peers,
+    int peer_count,
     size_t count,
     oo_dtype_t dtype,
     oo_reduce_op_t op,
+    oo_tuning_mode_t tuning_mode,
+    cudaStream_t stream);
+
+/* Reduce-scatter */
+oo_status_t oo_reduce_scatter_offset(
+    oo_node_t* node,
+    oo_buffer_t* local,
+    oo_buffer_t* const* peers,
+    int peer_count,
+    size_t element_offset,
+    size_t count,
+    oo_dtype_t dtype,
+    oo_reduce_op_t op,
+    oo_tensor_slice_t* out_slice,
+    cudaStream_t stream);
+
+oo_status_t oo_reduce_scatter(
+    oo_node_t* node,
+    oo_buffer_t* local,
+    oo_buffer_t* const* peers,
+    int peer_count,
+    size_t count,
+    oo_dtype_t dtype,
+    oo_reduce_op_t op,
+    oo_tensor_slice_t* out_slice,
+    cudaStream_t stream);
+
+oo_status_t oo_reduce_scatter_offset_tuned(
+    oo_node_t* node,
+    oo_buffer_t* local,
+    oo_buffer_t* const* peers,
+    int peer_count,
+    size_t element_offset,
+    size_t count,
+    oo_dtype_t dtype,
+    oo_reduce_op_t op,
+    oo_tuning_mode_t tuning_mode,
+    oo_tensor_slice_t* out_slice,
+    cudaStream_t stream);
+
+oo_status_t oo_reduce_scatter_tuned(
+    oo_node_t* node,
+    oo_buffer_t* local,
+    oo_buffer_t* const* peers,
+    int peer_count,
+    size_t count,
+    oo_dtype_t dtype,
+    oo_reduce_op_t op,
+    oo_tuning_mode_t tuning_mode,
+    oo_tensor_slice_t* out_slice,
+    cudaStream_t stream);
+
+/* All-gather */
+oo_status_t oo_all_gather_offset(
+    oo_node_t* node,
+    oo_buffer_t* local,
+    oo_buffer_t* const* peers,
+    int peer_count,
+    size_t element_offset,
+    size_t count,
+    oo_dtype_t dtype,
+    cudaStream_t stream);
+
+oo_status_t oo_all_gather(
+    oo_node_t* node,
+    oo_buffer_t* local,
+    oo_buffer_t* const* peers,
+    int peer_count,
+    size_t count,
+    oo_dtype_t dtype,
+    cudaStream_t stream);
+
+oo_status_t oo_all_gather_offset_tuned(
+    oo_node_t* node,
+    oo_buffer_t* local,
+    oo_buffer_t* const* peers,
+    int peer_count,
+    size_t element_offset,
+    size_t count,
+    oo_dtype_t dtype,
+    oo_tuning_mode_t tuning_mode,
+    cudaStream_t stream);
+
+oo_status_t oo_all_gather_tuned(
+    oo_node_t* node,
+    oo_buffer_t* local,
+    oo_buffer_t* const* peers,
+    int peer_count,
+    size_t count,
+    oo_dtype_t dtype,
     oo_tuning_mode_t tuning_mode,
     cudaStream_t stream);
 
