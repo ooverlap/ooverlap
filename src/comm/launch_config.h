ab2f0ed295553f34285265ad2755ede6beee987f
keyvan-dadash <ddssee5511@gmail.com>
2026-05-04 19:42:40 +0200

    refactore: add other plan to the launch config

--- src/comm/launch_config.h
+++ src/comm/launch_config.h
@@ -5,10 +5,47 @@
 namespace ooverlap {
 namespace comm {
 
-enum class AllreducePlanKind : int {
+enum class CollectivePlanFor : int {
+    AllReduce = 0,
+    ReduceScatter = 1,
+    AllGather = 2,
+};
+
+enum class AllReducePlanKind : int {
     TmaCopy = 0,
-    SeqFastGmem = 1,
-    OverlapFastGmem = 2,
+    SeqFastCopyGmem = 1,
+    OverlapFastCopyGmem = 2,
+};
+
+enum class ReduceScatterPlanKind : int {
+    TmaReduce = 0,
+    SeqFastAddGmem = 1,
+};
+
+enum class AllGatherPlanKind : int {
+    TmaCopy = 0,
+    SeqFastCopyGmem = 1,
+};
+
+union CollectivePlanKind {
+    AllReducePlanKind allreduce;
+    ReduceScatterPlanKind reduce_scatter;
+    AllGatherPlanKind all_gather;
+
+    __host__ __device__ constexpr CollectivePlanKind()
+        : allreduce(AllReducePlanKind::TmaCopy) {}
+
+    __host__ __device__ constexpr explicit CollectivePlanKind(
+        AllReducePlanKind value)
+        : allreduce(value) {}
+
+    __host__ __device__ constexpr explicit CollectivePlanKind(
+        ReduceScatterPlanKind value)
+        : reduce_scatter(value) {}
+
+    __host__ __device__ constexpr explicit CollectivePlanKind(
+        AllGatherPlanKind value)
+        : all_gather(value) {}
 };
 
 struct LaunchConfig {
@@ -19,14 +56,87 @@ struct LaunchConfig {
     int chunk_bytes = TMA_TWO_GPU_PEER_DEFAULT_CHUNK_BYTES;
     int stage_depth = TMA_TWO_GPU_PEER_DEFAULT_STAGE_DEPTH;
 
-    AllreducePlanKind plan_kind = AllreducePlanKind::TmaCopy;
+    CollectivePlanFor plan_for = CollectivePlanFor::AllReduce;
+    CollectivePlanKind plan{};
 };
 
 __host__ __device__ __forceinline__ LaunchConfig default_launch_config() {
     return LaunchConfig{};
 }
 
-__host__ __device__ __forceinline__ bool launch_config_valid(
+__host__ __device__ __forceinline__ LaunchConfig make_allreduce_launch_config(
+    AllReducePlanKind plan = AllReducePlanKind::TmaCopy) {
+    LaunchConfig config{};
+    config.plan_for = CollectivePlanFor::AllReduce;
+    config.plan = CollectivePlanKind(plan);
+    return config;
+}
+
+__host__ __device__ __forceinline__ LaunchConfig make_reduce_scatter_launch_config(
+    ReduceScatterPlanKind plan = ReduceScatterPlanKind::TmaReduce) {
+    LaunchConfig config{};
+    config.plan_for = CollectivePlanFor::ReduceScatter;
+    config.plan = CollectivePlanKind(plan);
+    return config;
+}
+
+__host__ __device__ __forceinline__ LaunchConfig make_all_gather_launch_config(
+    AllGatherPlanKind plan = AllGatherPlanKind::TmaCopy) {
+    LaunchConfig config{};
+    config.plan_for = CollectivePlanFor::AllGather;
+    config.plan = CollectivePlanKind(plan);
+    return config;
+}
+
+__host__ __device__ __forceinline__ void set_allreduce_plan(
+    LaunchConfig* config,
+    AllReducePlanKind plan) {
+    if (config == nullptr) {
+        return;
+    }
+
+    config->plan_for = CollectivePlanFor::AllReduce;
+    config->plan = CollectivePlanKind(plan);
+}
+
+__host__ __device__ __forceinline__ void set_reduce_scatter_plan(
+    LaunchConfig* config,
+    ReduceScatterPlanKind plan) {
+    if (config == nullptr) {
+        return;
+    }
+
+    config->plan_for = CollectivePlanFor::ReduceScatter;
+    config->plan = CollectivePlanKind(plan);
+}
+
+__host__ __device__ __forceinline__ void set_all_gather_plan(
+    LaunchConfig* config,
+    AllGatherPlanKind plan) {
+    if (config == nullptr) {
+        return;
+    }
+
+    config->plan_for = CollectivePlanFor::AllGather;
+    config->plan = CollectivePlanKind(plan);
+}
+
+__host__ __device__ __forceinline__ AllReducePlanKind allreduce_plan(
+    LaunchConfig config) {
+    return config.plan.allreduce;
+}
+
+__host__ __device__ __forceinline__ ReduceScatterPlanKind reduce_scatter_plan(
+    LaunchConfig config) {
+    return config.plan.reduce_scatter;
+}
+
+__host__ __device__ __forceinline__ AllGatherPlanKind all_gather_plan(
+    LaunchConfig config) {
+    return config.plan.all_gather;
+}
+
+__host__ __device__ __forceinline__ bool launch_config_valid_common(
     LaunchConfig config) {
     if (config.threads <= 0 || config.threads > 1024) {
         return false;
@@ -60,20 +170,54 @@ __host__ __device__ __forceinline__ bool launch_config_valid(
         return false;
     }
 
-    if (config.plan_kind == AllreducePlanKind::OverlapFastGmem) {
-        return config.max_ctas >= 2;
-    }
-    
     return true;
 }
 
-__host__ __device__ __forceinline__ bool launch_config_valid_for_overlap(
+__host__ __device__ __forceinline__ bool launch_config_valid_plan(
     LaunchConfig config) {
-    if (!launch_config_valid(config)) {
-        return false;
-    }
+    switch (config.plan_for) {
+        case CollectivePlanFor::AllReduce:
+            switch (config.plan.allreduce) {
+                case AllReducePlanKind::TmaCopy:
+                case AllReducePlanKind::SeqFastCopyGmem:
+                    return true;
 
-    return config.max_ctas >= TMA_TWO_GPU_PEER_OVERLAP_BLOCKS_PER_CTA;
+                case AllReducePlanKind::OverlapFastCopyGmem:
+                    return config.max_ctas >= 2;
+
+                default:
+                    return false;
+            }
+
+        case CollectivePlanFor::ReduceScatter:
+            switch (config.plan.reduce_scatter) {
+                case ReduceScatterPlanKind::TmaReduce:
+                case ReduceScatterPlanKind::SeqFastAddGmem:
+                    return true;
+
+                default:
+                    return false;
+            }
+
+        case CollectivePlanFor::AllGather:
+            switch (config.plan.all_gather) {
+                case AllGatherPlanKind::TmaCopy:
+                case AllGatherPlanKind::SeqFastCopyGmem:
+                    return true;
+
+                default:
+                    return false;
+            }
+
+        default:
+            return false;
+    }
+}
+
+__host__ __device__ __forceinline__ bool launch_config_valid(
+    LaunchConfig config) {
+    return launch_config_valid_common(config) &&
+           launch_config_valid_plan(config);
 }
 
 } // namespace comm
