56b3408a790319a416b0f9e6d22195a4dcae1e67
keyvan-dadash <ddssee5511@gmail.com>
2026-07-07 21:00:10 +0200

    fix: remove the condition

--- src/comm/kernels/window_task_executor.cuh
+++ src/comm/kernels/window_task_executor.cuh
@@ -260,9 +260,9 @@ __device__ __forceinline__ void execute_window_task_stripe(
             return;
         }
 
-        if (local_task >= 2) {
-            return;
-        }
+/*        if (local_task >= 2) {*/
+            /*return;*/
+        /*}*/
 
         const task::WindowTask task = tasks[task_idx];
 
